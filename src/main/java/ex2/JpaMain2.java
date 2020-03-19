e36f98822deba529a69babbef1a7a434e393d6ea
Jeongsu Yang <mycat83@gmail.com>
2020-03-19 22:26:25 +0900

    영속성 컨텍스트

---
+++ src/main/java/ex2/JpaMain2.java
@@ -0,0 +1,32 @@
+package ex2;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain2 {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("ZZZZZZ");
+
+            System.out.println("====================");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
