d92e5114f07b551a0ff198b3a8dbbaa4ed7fa7f1
Jeongsu Yang <mycat83@gmail.com>
2020-03-20 15:11:25 +0900

    플러시

--- src/main/java/ex2/Flush.java
+++ src/main/java/ex2/Flush.java
@@ -5,7 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-public class JpaMain {
+public class Flush {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
@@ -15,16 +15,10 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member findMember1 = em.find(Member.class, 1L);
-            Member findMember2 = em.find(Member.class, 1L);
+            Member member = new Member(100L, "Member100");
+            em.persist(member);
 
-            System.out.println("result = " + (findMember1 == findMember2));
-
-            Member member1 = new Member(30L, "A");
-            Member member2 = new Member(40L, "B");
-
-            em.persist(member1);
-            em.persist(member2);
+            em.flush();
 
             System.out.println("====================");
 
---
+++ src/main/java/ex2/Persist.java
@@ -0,0 +1,40 @@
+package ex2;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Persist {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member findMember1 = em.find(Member.class, 1L);
+            Member findMember2 = em.find(Member.class, 1L);
+
+            System.out.println("result = " + (findMember1 == findMember2));
+
+            Member member1 = new Member(30L, "A");
+            Member member2 = new Member(40L, "B");
+
+            em.persist(member1);
+            em.persist(member2);
+
+            System.out.println("====================");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
