b6c5184a931c1b68e96f8997956f0f05ad963694
Jeongsu Yang <mycat83@gmail.com>
2020-03-22 16:40:32 +0900

    단방향 연관관계

---
+++ src/main/java/chap04/JpaMain.java
@@ -0,0 +1,59 @@
+package chap04;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("test");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // 저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeam(team);
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            // 조회
+            Member findMember = em.find(Member.class, member.getId());
+            Team findTeam = findMember.getTeam();
+
+            System.out.println("findTeam.name = " + findTeam.getName());
+
+            // 수정
+            Team teamB = new Team();
+            teamB.setName("TeamB");
+            em.persist(teamB);
+
+            findMember.setTeam(teamB);
+
+            // 조회
+            Member findMember2 = em.find(Member.class, member.getId());
+            Team findTeam2 = findMember2.getTeam();
+
+            System.out.println("findTeam2.name = " + findTeam2.getName());
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
