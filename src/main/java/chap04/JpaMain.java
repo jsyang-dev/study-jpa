acfe8af95126c336483a6faedf4ff81cfc5ab210
Jeongsu Yang <mycat83@gmail.com>
2020-03-22 23:01:16 +0900

    양방향 연관관계와 연관관계의 주인 1 - 기본

--- src/main/java/chap04/JpaMain.java
+++ src/main/java/chap04/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -41,11 +42,16 @@ public class JpaMain {
 
             findMember.setTeam(teamB);
 
+            em.flush();
+            em.clear();
+
             // 조회
             Member findMember2 = em.find(Member.class, member.getId());
-            Team findTeam2 = findMember2.getTeam();
+            List<Member> members = findMember2.getTeam().getMembers();
 
-            System.out.println("findTeam2.name = " + findTeam2.getName());
+            for (Member m : members) {
+                System.out.println("m = " + m.getUsername());
+            }
 
             tx.commit();
         } catch (Exception e) {
